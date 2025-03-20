bec83b5d79a12aac3db68881cd9b488ef5a19c7d
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2025-03-21 03:31:26 +0600

    Showing Loading UIs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NavBar />
-        <main className="p-5">{children}</main>
+        <main className="p-5">
+          <Suspense fallback={<p>Loading...</p>}>{children}</Suspense>
+        </main>
       </body>
     </html>
   );
